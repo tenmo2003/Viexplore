98a7bdd32e67a5414a574406b6d472282afb69df
tenmo2003 <tenmo2003@gmail.com>
2023-10-21 23:35:35 +0700

    added ngrok ignore header

--- src/helper/axiosService.js
+++ src/helper/axiosService.js
@@ -4,6 +4,9 @@ const baseURL = "http://192.168.0.101:8085/api/";
 
 const service = axios.create({
   baseURL: baseURL,
+  headers: {
+    "ngrok-skip-browser-warning": "69420",
+  }
 });
 
 export default service;
