429dee12be5045703c5d4e7abcb5268d6dff810c
DinhChinh170603 <21020288@vnu.edu.vn>
2023-10-24 11:41:42 +0700

    fix layout search bar, plus logic click-in/out, not set data yet

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -6,6 +6,7 @@ import {
   View,
   StyleSheet,
   Image,
+  Keyboard
 } from "react-native";
 import MapView, { Geojson, Marker } from "react-native-maps";
 import { vietnam, mapStyle } from "../helper/vietnam";
@@ -29,14 +30,14 @@ function MapScreen({ navigation }) {
   const [isModalVisible, setModalVisible] = useState(false);
 
   const [query, setQuery] = useState("");
-  const [haveResult, setHaveResult] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+  const [showResults, setShowResults] = useState(false);
 
   const onChange = (value) => {
     setQuery(value);
   };
 
   const onSearch = (searchTerm) => {
-    setQuery(searchTerm)
     console.log("", searchTerm);
   };
 
@@ -65,6 +66,21 @@ function MapScreen({ navigation }) {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const maximumAmountOfSearchResults = 10;
+    const results = data
+      .filter((item) => {
+        const searchTerm = query.toLowerCase();
+        const tenditich = item.ten_di_tich.toLowerCase();
+
+        return searchTerm && tenditich.startsWith(searchTerm); // && tenditich !== searchTerm;
+        // điều kiện này là để khi tìm được chính xác tên thì nó sẽ không hiển thị ở
+      })
+      .slice(0, maximumAmountOfSearchResults);
+    setSearchResults(results);
+    setShowResults(results.length !== 0);
+  }, [query]);
+
   if (loading) {
     return <Loading />;
   }
@@ -85,6 +101,7 @@ function MapScreen({ navigation }) {
         pitchEnabled={false}
         rotateEnabled={false}
         moveOnMarkerPress={false}
+        onPress={Keyboard.dismiss}
       >
         <Geojson
           geojson={vietnam}
@@ -197,7 +214,11 @@ function MapScreen({ navigation }) {
           </View>
         </Modal>
       )}
-      <View style={styles.searchContainerEmpty}>
+      <View
+        style={
+          showResults ? styles.searchContainer : styles.searchContainerEmpty
+        }
+      >
         <View style={styles.searchInner}>
           <TextInput
             placeholder="Tìm kiếm..."
@@ -206,27 +227,26 @@ function MapScreen({ navigation }) {
             onChangeText={(value) => {
               setQuery(value);
             }}
+            onFocus={() => setShowResults(true)}
+            onEndEditing={() => setShowResults(false)}
           />
-          
-          <View style={styles.dropDown}>
-            {data
-              .filter((item) => {
-                const searchTerm = query.toLowerCase();
-                const tenditich = item.ten_di_tich.toLowerCase();
 
-                return searchTerm && tenditich.startsWith(searchTerm);// && tenditich !== searchTerm; 
-                // điều kiện này là để khi tìm được chính xác tên thì nó sẽ không hiển thị ở
-              })
-              .map((item, index) => (
-                <View
-                  onPress={() => onSearch(item.ten_di_tich)}
+          {showResults && (
+            <View style={styles.dropDown}>
+              {searchResults.map((item, index) => (
+                <TouchableOpacity //Touch? yes //ngon r
+                  onPress={() => {
+                    onSearch(item.ten_di_tich);
+                    setShowResults(false);
+                  }}
                   style={styles.dropDownRow}
                   key={index}
                 >
                   <Text>{item.ten_di_tich}</Text>
-                </View>
+                </TouchableOpacity>
               ))}
-          </View>
+            </View>
+          )}
         </View>
       </View>
     </View>
@@ -387,23 +407,29 @@ const styles = StyleSheet.create({
     borderRadius: 35,
     display: "flex",
     height: 40,
+    borderColor: "#BABABA",
   },
   searchContainer: {
     position: "absolute",
     top: 60,
     backgroundColor: "white",
     width: searchBarWidth,
-    borderTopLeftRadius: 35,
-    borderTopRightRadius: 35,
+    borderTopLeftRadius: 28,
+    borderTopRightRadius: 28,
     display: "flex",
     height: 40,
+    borderBottomWidth: 1,
+    borderColor: "#BABABA",
   },
   dropDown: {
     position: "absolute",
-    top: 50,
+    top: 40,
     backgroundColor: "white",
-    borderRadius: 15,
-    width: dropDownWidth,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+
+    // borderRadius: 15, // xoa cai nay di r set border ben duoi ko la cai nay override
+    width: searchBarWidth,
     display: "flex",
     flexDirection: "column",
   },
@@ -417,7 +443,9 @@ const styles = StyleSheet.create({
   },
   input: {
     position: "absolute",
+
     padding: 12,
+    paddingLeft: 15,
     justifyContent: "center",
     alignItems: "center",
     width: searchBarWidth,
@@ -427,5 +455,3 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
-
-
