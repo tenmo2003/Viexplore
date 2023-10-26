4818f9e54030f342765390bf9f7bdcd024580626
ngocminh1605 <100121817+ngocminh1605@users.noreply.github.com>
2023-10-26 11:08:24 +0700

    profile

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -1,18 +1,140 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "react-native-vector-icons";
+import { Text, View, StyleSheet, SafeAreaView, ScrollView, Image } from "react-native";
+import { BottomTabBarHeightCallbackContext, BottomTabBarHeightContext } from "@react-navigation/bottom-tabs";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-const UserScreen = ({ route, navigation }) => {
+
+const BottomTab = () => {
+  const Tab = createMaterialTopTabNavigator();
+  const Post = () => {
+    return (
+      <View >
+        <Text>
+          Post
+        </Text>
+      </View>
+    )
+  }
+  const Forums = () => {
+    return (
+      <View>
+        <Text>
+          Forums
+        </Text>
+      </View>
+    )
+  }
   return (
-    <View className="flex-1 justify-center items-center bg-cyan-100">
-      <Text>User</Text>
-      <TouchableOpacity
-        className="bg-red-500 p-3"
-        onPress={() => navigation.navigate("Login")}
-      >
-        <Text className="text-white">Map</Text>
-      </TouchableOpacity>
+    <Tab.Navigator style={{ marginTop: -450 }}
+      screenOptions={({ route }) => ({
+        tabBarShowLabel: false,
+        tabBarIndicatorStyle: {
+          backgroundColor: 'black',
+          height: 1.5
+        },
+        tabBarIcon: ({ focused, color }) => {
+          let iconName;
+          if (route.name === "Post") {
+            iconName = focused ? "bookmarks" : "bookmarks";
+            color = focused ? "#52575D" : "#AEB5BC";
+          } else if (route.name === "Save") {
+            iconName = focused ? "heart" : "heart";
+            color = focused ? "#52575D" : "#AEB5BC";
+          }
+          return <Ionicons name={iconName} color={color} size={23} />;
+        }
+      })}>
+      <Tab.Screen name="Post" component={Post}></Tab.Screen>
+      <Tab.Screen name="Save" component={Forums}></Tab.Screen>
+    </Tab.Navigator>
+  )
+}
+
+
+const UserScreen = ({route, navigation}) => {
+  return (
+    <View style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={true}>
+        <View>
+        
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Login")}
+          >
+            <Ionicons name="settings-outline" size={23} color="#52575D" style={{ marginTop: 20, left: 350 }}></Ionicons>
+          </TouchableOpacity>
+        
+        </View>
+        
+        <View style={{ alignSelf: "center" }}>
+          <View style={styles.profileImage}>
+            <Image source={require('./../../assets/cho.jpg')} style={styles.image} resizeMode="center"></Image>
+          </View>
+          <View style={styles.add}>
+            <Ionicons name="ios-add" size={23} color="#DFD8C8" style={{ marginTop: -2, marginLeft: -1 }}></Ionicons>
+          </View>
+        </View>
+        <View style={styles.info}>
+          <Text style={[styles.text, { fontWeight: "200", fontSize: 20, fontWeight: 'bold' }]}>MingMing</Text>
+          <Text style={[styles.text, { color: "#AEB5BC", fontSize: 14, fontStyle: 'italic', marginBottom: 5 }]}>Đ thích code</Text>
+        </View>
+      </ScrollView>
+      <BottomTab />
     </View>
+
+
   );
 };
 
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      backgroundColor: "#ffff",
+    },
+
+    text: {
+      // fontFamily: 'Cochin',
+      color: "#52575D",
+      top: 5
+    },
+    image: {
+      flex: 1,
+      width: undefined,
+      height: undefined,
+      borderRadius: 100
+    },
+    titleBar: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+
+      marginHorizontal: 16
+    },
+    profileImage: {
+      marginTop: 50,
+      width: 100,
+      height: 100,
+      overflow: "hidden",
+
+    },
+    add: {
+      backgroundColor: "#41444B",
+      position: "absolute",
+      left: 70,
+      top: 130,
+      width: 20,
+      height: 20,
+      borderRadius: 30,
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    info: {
+      position: "relative",
+      alignSelf: "center",
+      alignItems: "center",
+    },
+
+  }
+)
 export default UserScreen;
