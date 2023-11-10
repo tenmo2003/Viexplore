0021b86acb2b381f0bb27aa3125f61d04a2159d3
hothugiang <21020309@vnu.edu.vn>
2023-11-11 01:06:34 +0700

    Create post & get topic list forum

--- src/screens/CreatePostScreen.js
+++ src/screens/CreatePostScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { Platform } from "react-native";
 import {
   Text,
@@ -13,6 +13,10 @@ import {
 import { Icon } from "react-native-elements";
 import { Ionicons } from "react-native-vector-icons";
 import * as ImagePicker from 'expo-image-picker';
+import Loading from "../components/Loading";
+import { showAlert } from "../helper/CustomAlert";
+import service from "../helper/axiosService";
+import { useNavigation } from '@react-navigation/native';
 
 function CreatePostScreen ({ navigation }) {
     const [selectedImages, setSelectedImages] = useState([]);
@@ -45,77 +49,164 @@ function CreatePostScreen ({ navigation }) {
         setSelectedImages(updatedImages);
     };
 
-    return (
-        <ScrollView>
-            <View style={styles.sheetScreen}>
-                <View style={{ flexDirection: "row", justifyContent: 'space-between', paddingRight: 15, paddingLeft: 10, paddingTop: 10 }}>
-                    <TouchableOpacity>
-                        <Icon name="chevron-left" type="font-awesome" color="#000" size={24} onPress={() => navigation.navigate("Forum")}/>
-                    </TouchableOpacity>
-                    <Text style={{ fontSize: 18, alignItems:"center", paddingLeft: 15 }}> Tạo bài viết</Text>
-                    <TouchableOpacity>
-                        <Text style={{ fontSize: 18, color: "#687DAA", fontWeight: "bold" }}>Đăng</Text>
-                    </TouchableOpacity>
-                    </View>
+    //logic
+    const [name, setName] = useState("");
+    const [content, setContent] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const onSubmit = () => {
+        console.log("name: ", name);
+        console.log("content: ", content);
+        console.log("images: ", selectedImages);
+        
+        const formData = new FormData();
+        formData.append("name", name);
+        formData.append("content", content);
+        for (let i = 0; i < selectedImages.length; i++) {
+            formData.append("images", {
+                uri:
+                  Platform.OS === "android"
+                    ? selectedImages[i].uri
+                    : selectedImages[i].uri.replace("file://", "/private"),
+                name: "avatar.jpg",
+                type: "image/jpg",
+              });
+        }
+
+        setLoading(true);
+        service
+            .post("/topic", formData)
+            .then(
+                (res) => {
+                    console.log("Message: " + res.data.message);
+                    setLoading(false);
+                    if (res.data.name) {
+                        console.log("Name: " + res.data.name);
+                    }
+                
+                    if (res.data.content) {
+                    console.log("Content: " + res.data.content);
+                    }
+
+                    navigation.navigate('Forum');
+                })
+            .catch((error) => {
+                console.log("Network failed", error);
+                setLoading(false);
+            });
             
-                    <View style={styles.flexColumn}>
-                        <View style={{ flexDirection: "row", marginBottom: 15 }}>
-                            <View style={styles.profileImage}>
-                                <Image
-                                source={require("./../../assets/cho.jpg")}
-                                style={styles.image}
-                                resizeMode="center"
-                                ></Image>
-                            </View>
-                            <View>
-                                <Text style={styles.Name}>MingMing</Text>
-                                <Text style={styles.Time}>Username vì t bí layout</Text>
-                            </View>
-                        </View>
+            
+    };
 
-                        <View style={styles.center}>
-                        <View style={styles.postContent}>
-                            <TextInput
-                            multiline={true}
-                            maxLength={400}
-                            style={styles.textPost}
-                            placeholder="Hãy thêm kỷ niệm đẹp...."
-                            ></TextInput>
-                            <Icon
-                            type="font-awesome"
-                            name="image"
-                            color="#BABABA"
-                            size={30}
-                            containerStyle={{ bottom: 10, right:10,position:"absolute"}}
-                            onPress={pickMultipleImages}
-                            />
+    const onChangeName = (input) => {
+        setName(input);
+    };
+    
+    const onChangeContent = (input) => {
+        setContent(input);
+    };
+
+    const [fullname, setFullName] = useState("");
+    const [username, setUsername] = useState("");
+    const [avatar, setAvatar] = useState(null);
+    useEffect(() => {
+        setLoading(true);
+        service.get("/users/me", {}).then(
+          (res) => {
+            const userData = res.data.results;
+            setFullName(userData.fullName);
+            setUsername(userData.username);
+            setAvatar(userData.avatar);
+            setLoading(false);
+          },
+          () => {
+            console.log("failed");
+          }
+        );
+    }, []);
+
+    return (
+        <View>
+            {loading && <Loading />}
+            <ScrollView>
+                <View style={styles.sheetScreen}>
+                    <View style={{ flexDirection: "row", justifyContent: 'space-between', paddingRight: 15, paddingLeft: 10, paddingTop: 10 }}>
+                        <TouchableOpacity>
+                            <Icon name="chevron-left" type="font-awesome" color="#000" size={24} onPress={() => navigation.navigate("Forum")}/>
+                        </TouchableOpacity>
+                        <Text style={{ fontSize: 18, alignItems:"center", paddingLeft: 15 }}> Tạo bài viết</Text>
+                        <TouchableOpacity onPress={() => onSubmit()}>
+                            <Text style={{ fontSize: 18, color: "#687DAA", fontWeight: "bold" }} >Đăng</Text>
+                        </TouchableOpacity>
                         </View>
-                        
-                        {selectedImages && (
-                            <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "center",}}>
-                            {selectedImages.map((image, index) => (
-                                <View key={index} style={{ marginLeft: 5, marginBottom: 5, position: 'relative' }}>
-                                <Image source={{ uri: image.uri }} style={styles.postimg} />
-                                <TouchableOpacity
-                                    style={styles.closeButton}
-                                    onPress={() => handleDeleteImage(index)}
-                                >
-                                    <Ionicons name="close-outline" size={18} color="white"></Ionicons>
-                                </TouchableOpacity>
+                
+                        <View style={styles.flexColumn}>
+                            <View style={{ flexDirection: "row", marginBottom: 15 }}>
+                                <View style={styles.profileImage}>
+                                    <Image
+                                    source={avatar ? { uri: avatar } : require("./../../assets/ava.png")}
+                                    style={styles.image}
+                                    resizeMode="center"
+                                    ></Image>
                                 </View>
-                            ))}
+                                <View>
+                                    <Text style={styles.Name}>{fullname}</Text>
+                                    <Text style={styles.Time}>{username}</Text>
+                                </View>
                             </View>
-                        )}
 
-                        <View style={styles.containerButton}>
-                            <TouchableOpacity style={styles.button}>
-                            <Text style={styles.buttonText}>Đăng</Text>
-                            </TouchableOpacity>
+                            <View style={styles.center}>
+                            <View style={{...styles.postContent, height: 50,}}>
+                                <TextInput
+                                    placeholder="Tiêu đề:"
+                                    style={styles.textPost}
+                                    onChangeText={(value) => onChangeName(value)}
+                                />
+                            </View>
+                            <View style={styles.postContent}>
+                                <TextInput
+                                multiline={true}
+                                maxLength={400}
+                                style={styles.textPost}
+                                placeholder="Hãy thêm kỷ niệm đẹp...."
+                                onChangeText={(value) => onChangeContent(value)}
+                                ></TextInput>
+                                <Icon
+                                type="font-awesome"
+                                name="image"
+                                color="#BABABA"
+                                size={30}
+                                containerStyle={{ bottom: 10, right:10,position:"absolute"}}
+                                onPress={pickMultipleImages}
+                                />
+                            </View>
+                            
+                            {selectedImages && (
+                                <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "center",}}>
+                                {selectedImages.map((image, index) => (
+                                    <View key={index} style={{ marginLeft: 5, marginBottom: 5, position: 'relative' }}>
+                                    <Image source={{ uri: image.uri }} style={styles.postimg} />
+                                    <TouchableOpacity
+                                        style={styles.closeButton}
+                                        onPress={() => handleDeleteImage(index)}
+                                    >
+                                        <Ionicons name="close-outline" size={18} color="white"></Ionicons>
+                                    </TouchableOpacity>
+                                    </View>
+                                ))}
+                                </View>
+                            )}
+
+                            <View style={styles.containerButton}>
+                                <TouchableOpacity style={styles.button} onPress={() => onSubmit()}>
+                                    <Text style={styles.buttonText}>Đăng</Text>
+                                </TouchableOpacity>
+                            </View>
                         </View>
                     </View>
                 </View>
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </View>
     )
 }
 
